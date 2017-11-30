0fe19209df2256089d95b592197659fadd959959
Adair Hawkins <adairhawkins@gmail.com>
2017-11-29 19:07:28 -0600

    connecting server
    created controller.js for routes
    created .gitignore for github management
    created package.json for npm install and server connection
    created server.js for connection to Heroku

---
+++ controllers/controller.js
@@ -0,0 +1,14 @@
+//Pull all requirements
+var express = require('express');
+//var db = require("../models");
+var router = express.Router();
+var path = require('path');
+
+
+//Web page entry
+router.get('/', function(req,res){
+    res.send('this is the home page');
+});
+
+//Serves the home page
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+//const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+//const exphbs = require('express-handlebars');
+
+
+var app = express();
+//app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// app.use(methodOverride('_method'));
+// app.engine('handlebars', exphbs({
+//     defaultLayout: 'main'
+// }));
+// app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/controller.js');
+app.use('/', routes);
+
+var PORT = 3001;
+app.listen(process.env.PORT || 3001);
